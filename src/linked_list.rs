ae574237008ad25f10b69a3e8d146370c0f94fbf
harsh <66548698+harsssh@users.noreply.github.com>
2024-05-12 07:38:33 +0900

    add tests

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -45,11 +45,11 @@ impl<T> LinkedList<T> {
         unimplemented!()
     }
 
-    pub fn pop_front() -> Option<T> {
+    pub fn pop_front(&mut self) -> Option<T> {
         unimplemented!()
     }
 
-    pub fn pop_back() -> Option<T> {
+    pub fn pop_back(&mut self) -> Option<T> {
         unimplemented!()
     }
 }
@@ -59,19 +59,62 @@ mod tests {
     use super::*;
 
     #[test]
+    fn new() {
+        let list: LinkedList<i32> = LinkedList::new();
+
+        assert_eq!(list.head, None);
+        assert_eq!(list.tail, None);
+    }
+
+    #[test]
     fn push_front() {
         let mut list = LinkedList::new();
-        list.push_front(3);
-        list.push_front(2);
         list.push_front(1);
-        let want = {
-            let n3 = Node::new(3);
-            let r3 = Rc::new(RefCell::new(n3));
-            let n2 = Node { data: 2, next: Some(r3.clone()) };
-            let n1 = Node { data: 1, next: Some(Rc::new(RefCell::new(n2))) };
-            LinkedList { head: Some(Rc::new(RefCell::new(n1))), tail: Some(r3) }
-        };
+        list.push_front(2);
+        list.push_front(3);
 
-        assert_eq!(list, want);
+        let head = list.head.as_ref().unwrap();
+        assert_eq!(head.borrow().data, 3);
+        assert_eq!(head.borrow().next.as_ref().unwrap().borrow().data, 2);
+        assert_eq!(list.tail.as_ref().unwrap().borrow().data, 1);
+    }
+
+    #[test]
+    fn push_back() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        let head = list.head.as_ref().unwrap();
+        assert_eq!(head.borrow().data, 1);
+        assert_eq!(head.borrow().next.as_ref().unwrap().borrow().data, 2);
+        assert_eq!(list.tail.as_ref().unwrap().borrow().data, 3);
+    }
+
+    #[test]
+    fn pop_front() {
+        let mut list = LinkedList::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+
+        assert_eq!(list.pop_front(), Some(3));
+        assert_eq!(list.pop_front(), Some(2));
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn pop_back() {
+        let mut list = LinkedList::new();
+        list.push_back(1);
+        list.push_back(2);
+        list.push_back(3);
+
+        assert_eq!(list.pop_back(), Some(3));
+        assert_eq!(list.pop_back(), Some(2));
+        assert_eq!(list.pop_back(), Some(1));
+        assert_eq!(list.pop_back(), None);
     }
 }
